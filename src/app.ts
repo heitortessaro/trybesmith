50cb9bc7ed27adf19f78e9c8617179d65da0f078
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-29 14:59:55 -0300

    ci: Add error middlewere.
    Using express-async-errors to catch the errors.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import 'express-async-errors';
 
 const app = express();
 
@@ -7,6 +8,28 @@ app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ info: 'working' });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  // console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+
+  next();
 });
 
 export default app;
